f35b949b44e57ee07087897032b104ef04badeb0
keybraker <iantsiakkas@gmail.com>
2017-08-10 02:04:08 +0300

    good start

---
+++ Makefile
@@ -0,0 +1,13 @@
+CXX=g++
+CXXFLAGS=-O2 -pedantic -Wall -Wextra -ansi -std=c++11
+
+all: jpegOrganizer
+
+exif.o: jpegOrganizerFiles/exif.cpp
+	$(CXX) $(CXXFLAGS) -c jpegOrganizerFiles/exif.cpp
+
+jpegOrganizer: exif.o jpegOrganizerFiles/jpegOrganizer.cpp
+	$(CXX) $(CXXFLAGS) -o jpegOrganizer exif.o jpegOrganizerFiles/jpegOrganizer.cpp
+
+clean:
+	rm -f *.o jpegOrganizer jpegOrganizer.exe
---
+++ jpegOrganizerFiles/jpegOrganizer.cpp
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include "exif.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string>
+#include <iostream>
+#include <dirent.h> 
+#include <stdio.h>
+
+int is_regular_file(const char *path){
+
+    struct stat path_stat;
+    stat(path, &path_stat);
+    return S_ISREG(path_stat.st_mode);
+}
+
+int main(int argc, char *argv[]) {
+
+  if(argc == 1){
+    printf("Usage: jpegOrganizer <jpeg file>\n");
+    return -1;
+
+  }else if (argc == 2) {
+    if(is_regular_file(argv[1]) == 1){
+
+      printf("jpeg version\n");
+      return 0;
+
+    }else{
+
+      printf("folder version\n");
+      
+      DIR           *d;
+      struct dirent *dir;
+      d = opendir(argv[1]);
+      
+      if (d) {
+        while ((dir = readdir(d)) != NULL){
+          printf("%s\n", dir->d_name);
+        }
+        closedir(d);
+        
+      }
+
+      return 0;
+
+
+    }
+
+  } else {
+    printf("Too much info\n");
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Read the JPEG file into a buffer
+  FILE *fp = fopen(argv[1], "rb");
+  if (!fp) {
+    printf("Can't open file.\n");
+    return -1;
+  }
+  fseek(fp, 0, SEEK_END);
+  unsigned long fsize = ftell(fp);
+  rewind(fp);
+  unsigned char *buf = new unsigned char[fsize];
+  if (fread(buf, 1, fsize, fp) != fsize) {
+    printf("Can't read file.\n");
+    delete[] buf;
+    return -2;
+  }
+  fclose(fp);
+
+  // Parse EXIF
+  easyexif::EXIFInfo result;
+  int code = result.parseFrom(buf, fsize);
+  delete[] buf;
+  if (code) {
+    printf("Error parsing EXIF: code %d\n", code);
+    return -3;
+  }
+
+
+
+  // Dump EXIF information
+  printf("Original date/time   : %s\n", result.DateTimeOriginal.c_str());
+
+  return 0;
+}
