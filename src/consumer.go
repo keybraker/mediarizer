73fa17cf0629a390755855995ae8cc30f3f3a9bb
Keybraker <iantsiakkas@gmail.com>
2023-02-24 09:38:45 +0200

    Make move file rename file in order for it to not create copies

--- src/consumer.go
+++ src/consumer.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 )
@@ -48,34 +47,48 @@ func consumer(destinationPath string, queue <-chan FileInfo, done chan<- struct{
 	done <- struct{}{}
 }
 
-func moveFile(sourcePath, destPath string) error {
-	// Open the source file.
-	sourceFile, err := os.Open(sourcePath)
-	if err != nil {
-		return fmt.Errorf("failed to open source file %s: %v", sourcePath, err)
-	}
-	defer sourceFile.Close()
+// func moveFile(sourcePath, destPath string) error {
+// 	// Open the source file.
+// 	sourceFile, err := os.Open(sourcePath)
+// 	if err != nil {
+// 		return fmt.Errorf("failed to open source file %s: %v", sourcePath, err)
+// 	}
+// 	defer sourceFile.Close()
 
+// 	// Create the destination file's directory if it doesn't exist.
+// 	if err := os.MkdirAll(filepath.Dir(destPath), os.ModePerm); err != nil {
+// 		return fmt.Errorf("failed to create destination directory %s: %v", filepath.Dir(destPath), err)
+// 	}
+
+// 	// Create the destination file.
+// 	destFile, err := os.Create(destPath)
+// 	if err != nil {
+// 		return fmt.Errorf("failed to create destination file %s: %v", destPath, err)
+// 	}
+// 	defer destFile.Close()
+
+// 	// Copy the contents of the source file to the destination file.
+// 	if _, err := io.Copy(destFile, sourceFile); err != nil {
+// 		return fmt.Errorf("failed to copy contents of %s to %s: %v", sourcePath, destPath, err)
+// 	}
+
+// 	// Delete the source file.
+// 	if err := os.Remove(sourcePath); err != nil {
+// 		return fmt.Errorf("failed to delete source file %s: %v", sourcePath, err)
+// 	}
+
+// 	return nil
+// }
+
+func moveFile(sourcePath, destPath string) error {
 	// Create the destination file's directory if it doesn't exist.
 	if err := os.MkdirAll(filepath.Dir(destPath), os.ModePerm); err != nil {
 		return fmt.Errorf("failed to create destination directory %s: %v", filepath.Dir(destPath), err)
 	}
 
-	// Create the destination file.
-	destFile, err := os.Create(destPath)
-	if err != nil {
-		return fmt.Errorf("failed to create destination file %s: %v", destPath, err)
-	}
-	defer destFile.Close()
-
-	// Copy the contents of the source file to the destination file.
-	if _, err := io.Copy(destFile, sourceFile); err != nil {
-		return fmt.Errorf("failed to copy contents of %s to %s: %v", sourcePath, destPath, err)
-	}
-
-	// Delete the source file.
-	if err := os.Remove(sourcePath); err != nil {
-		return fmt.Errorf("failed to delete source file %s: %v", sourcePath, err)
+	// Rename the source file to the destination file.
+	if err := os.Rename(sourcePath, destPath); err != nil {
+		return fmt.Errorf("failed to move file from %s to %s: %v", sourcePath, destPath, err)
 	}
 
 	return nil
